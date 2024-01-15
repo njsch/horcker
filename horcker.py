6d244d6f07d2827020b9aed7bdb01cbe8854ed1a
Nathaniel Schmidt <schmidty2244@gmail.com>
2024-01-16 01:52:01 +1100

    Add clarifying comments for lists.

--- horcker.py
+++ horcker.py
@@ -84,16 +84,16 @@ numChaps = (50, 40, 27, 36, 34, 24, 21, 4, 31, 24, 22, 25, 29, 36, 10, 13, 10, 4
 
 # Lists data:
 lists = {
-	"One" : [numChaps[39], numChaps[40], numChaps[41], numChaps[42]],
-	"Two" : [numChaps[0], numChaps[1], numChaps [2], numChaps [3], numChaps[4]],
-	"Three" : [numChaps[44], numChaps[45], numChaps[46], numChaps[47], numChaps[48], numChaps[49], numChaps[50], numChaps[57]],
-	"Four" : [numChaps[51], numChaps[52], numChaps[53], numChaps[54], numChaps[55], numChaps[56], numChaps[58], numChaps[59], numChaps[60], numChaps[61], numChaps[62], numChaps[63], numChaps[64], numChaps[65]],
-	"Five" : [numChaps[17], numChaps[20], numChaps [21]],
-	"Six" : [numChaps[18]],
-	"Seven" : [numChaps[19]],
-	"Eight" : [numChaps[5], numChaps[6], numChaps[7], numChaps[8], numChaps[9], numChaps[10], numChaps[11], numChaps[12], numChaps[13], numChaps[14], numChaps[15], numChaps[16]],
-	"Nine" : [numChaps[22], numChaps[23], numChaps[24], numChaps[25], numChaps[26], numChaps[27], numChaps[28], numChaps[29], numChaps[30], numChaps[31], numChaps[32], numChaps[33], numChaps[34], numChaps[35], numChaps[36], numChaps[37], numChaps[38]],
-	"Ten" : [numChaps[43]]
+	"One" : [numChaps[39], numChaps[40], numChaps[41], numChaps[42]],# canonical Gospels -- synoptics (Mat., Mark, Luk) and John.
+	"Two" : [numChaps[0], numChaps[1], numChaps [2], numChaps [3], numChaps[4]],# Torah/Pentateuch (Gen, Exod, Lev, Num, Deut).
+	"Three" : [numChaps[44], numChaps[45], numChaps[46], numChaps[47], numChaps[48], numChaps[49], numChaps[50], numChaps[57]],# Paul/Luke? (Rom, I Cor, II Cor, Gal, Eph, Phil, Col, Heb).
+	"Four" : [numChaps[51], numChaps[52], numChaps[53], numChaps[54], numChaps[55], numChaps[56], numChaps[58], numChaps[59], numChaps[60], numChaps[61], numChaps[62], numChaps[63], numChaps[64], numChaps[65]],# Basically the rest of the NT.
+	"Five" : [numChaps[17], numChaps[20], numChaps [21]],# Wisdom literature/poetry (with Psalms and Proverbs excepted as these are their own lists as indidivudal books).
+	"Six" : [numChaps[18]],# Psalms
+	"Seven" : [numChaps[19]],# Proverbs
+	"Eight" : [numChaps[5], numChaps[6], numChaps[7], numChaps[8], numChaps[9], numChaps[10], numChaps[11], numChaps[12], numChaps[13], numChaps[14], numChaps[15], numChaps[16]],# Former prophets/earlier Israelite history.
+	"Nine" : [numChaps[22], numChaps[23], numChaps[24], numChaps[25], numChaps[26], numChaps[27], numChaps[28], numChaps[29], numChaps[30], numChaps[31], numChaps[32], numChaps[33], numChaps[34], numChaps[35], numChaps[36], numChaps[37], numChaps[38]],# Latter prophets (major and minor)
+	"Ten" : [numChaps[43]]# Acts.
 }
 
 def getBookName (indx):
@@ -126,7 +126,7 @@ def getBookIndx (name):
 		@rtype: int
 	"""
 	
-	bookIndx = 1# Canonical, not computational/Pythonic!
+	bookIndx = 1# Canonical index, not computational/Pythonic!
 	for i in books:
 		if bookIndx == len(books):
 			print ("Error: book not found. Please make sure you typed the name correctly or that it is a valid English-rendered cononical book name (Apocryphal literature not used).")
